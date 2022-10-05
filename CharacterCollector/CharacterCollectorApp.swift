f6fc09b1e6a9bb797cbe117f1dfd4dda5819675c
kai_lee <haesong97@gmail.com>
2022-10-05 04:17:06 -0500

    Initial commit

---
+++ CharacterCollector/CharacterCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterCollectorApp.swift
+//  CharacterCollector
+//
+//  Created by Haesong Lee on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CharacterCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CharacterCollector/JikanModel.swift
@@ -0,0 +1,39 @@
+//
+//  JikanModel.swift
+//  CharacterCollector
+//
+//  Created by Haesong Lee on 10/5/22.
+//
+
+import SwiftUI
+
+let defaultText: String = "N/A"
+struct JikanModel: Hashable {
+    
+    var characterId: Int = 0
+    var characterName: String  = defaultText
+    var title: String = defaultText
+    var imageJpg: String = defaultText
+    
+    var imageUrl: URL? {return URL(string: imageJpg)}
+    
+    init(json: [String:Any]) {
+        if let data = json["data"] as? [String:Any] {
+            if let name = data["name"] {self.characterName = "\(name)"}
+            if let images = data["images"] as? [String:Any] {
+                if let jpg = images["jpg"] as? [String:Any] {
+                    if let image = jpg["image_url"] {self.imageJpg = "\(image)"}
+                }
+            }
+            if let anime = data["anime"] as? [[String:Any]] {
+                if let details = anime.first?["anime"] as? [String:Any] {
+                    if let title = details["title"] {self.title = "\(title)"}
+                }
+            } else if let manga = data["manga"] as? [[String:Any]] {
+                if let details = manga.first?["manga"] as? [String:Any] {
+                    if let title = details["title"] {self.title = "\(title)"}
+                }
+            }
+        }
+    }
+}
