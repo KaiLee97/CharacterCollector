764ff6d1b3282f3f7729193fe409de9f6e2d07c5
Kai Lee <haesong97@gmail.com>
2023-03-16 04:04:00 -0500

    Targeting for ios 15

--- CharacterCollector/Views/ClaimedListView.swift
+++ CharacterCollector/Views/ClaimedListView.swift
@@ -11,25 +11,23 @@ import SwiftUI
 struct ClaimedListView: View {
     @State var tappedCharacter: Bool = false
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color.black.opacity(0.95).ignoresSafeArea()
-                VStack {
-                    Text("Claimed Characters")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(Color.white)
-                        .padding(.vertical, 16)
-                    ScrollView(.vertical) {
-                        LazyVStack(alignment: .center, spacing: 0) {
-                            ForEach(CharacterManager.shared.claimedList) { char in
-                                ClaimedCharacterTile(character: char)
-                            }
+        ZStack {
+            Color.black.opacity(0.95).ignoresSafeArea()
+            VStack {
+                Text("Claimed Characters")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.white)
+                    .padding(.vertical, 16)
+                ScrollView(.vertical) {
+                    LazyVStack(alignment: .center, spacing: 0) {
+                        ForEach(CharacterManager.shared.claimedList) { char in
+                            ClaimedCharacterTile(character: char)
                         }
-                        .padding(.horizontal, 12)
                     }
-                    Spacer()
+                    .padding(.horizontal, 12)
                 }
+                Spacer()
             }
         }
     }
